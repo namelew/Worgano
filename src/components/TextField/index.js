074e090b94cb3143ad58d8a80cfc247f8f1987ae
namelew <diogomaciel.cunha@gmail.com>
2023-07-19 08:35:36 -0300

    Criando stateless input de dados

--- src/components/TextField/index.js
+++ src/components/TextField/index.js
@@ -3,10 +3,17 @@ import "./TextField.css"
 const TextField = (props) => {
     const scapedPlaceholder = `${props.placeholder}...`
 
+    let value = ''
+
+    const onTyping = (event) => {
+        value = event.target.value
+        console.log(value)
+    }
+
     return (
         <div className="text-field">
             <label>{props.label}</label>
-            <input required={props.required} placeholder={scapedPlaceholder}></input>
+            <input value={value} onChange={onTyping} required={props.required} placeholder={scapedPlaceholder}></input>
         </div>
     )
 }
