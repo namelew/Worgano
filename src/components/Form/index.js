0d68d94768ac869a5e0567b37de69b688f44222d
namelew <diogomaciel.cunha@gmail.com>
2023-07-19 11:51:46 -0300

    Limpando formulário e removendo display de times vazios

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -18,6 +18,10 @@ const Form = (props) => {
             image,
             team
         })
+
+        setImage('')
+        setName('')
+        setPosition('')
     }
 
     return (
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -3,12 +3,14 @@ import "./Team.css"
 
 const Team = (props) => {
     return (
+        (props.collaborators.length > 0) ?
         <section className="team" style={{ backgroundColor: props.backgroundColor }}>
             <h3 style={{ borderColor: props.color }}>{props.name}</h3>
             <div className="collaborators">
                 {props.collaborators.map( collaborator => <Collaborator name={collaborator.name} position={collaborator.position} img={collaborator.image}/>)}
             </div>
         </section>
+        : ''
     )
 }
 
