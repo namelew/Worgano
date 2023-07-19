3ecab554b63e9518611dd160d76103b4a2b2dd5a
namelew <diogomaciel.cunha@gmail.com>
2023-07-19 11:16:39 -0300

    Criando base do card de Colaborador

---
+++ src/components/Collaborator/index.js
@@ -0,0 +1,17 @@
+import './Collaborator.css'
+
+const Collaborator = (props) => {
+    return (
+        <div className='collaborator'>
+            <div className='cabecalho'>
+                <img src='https://github.com/namelew.png' alt='Diogo Maciel da Cunha'/>
+            </div>
+            <div className='rodape'>
+                <h4>Diogo Maciel da Cunha</h4>
+                <h5>Aluno</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Collaborator;
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,9 +1,11 @@
+import Collaborator from "../Collaborator";
 import "./Team.css"
 
 const Team = (props) => {
     return (
         <section className="team" style={{ backgroundColor: props.backgroundColor }}>
             <h3 style={{ borderColor: props.color }}>{props.name}</h3>
+            <Collaborator/>
         </section>
     )
 }
